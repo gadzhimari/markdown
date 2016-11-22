5dcadd7eba401b5cf4215953e3aa3cd1fbd7c696
Nikita Gusakov <gusnkt@gmail.com>
2016-11-22 15:55:09 +0300

    refactor: remove markdown-it

--- src/index.js
+++ src/index.js
@@ -2,26 +2,10 @@
  * Copyright 2016 Dialog LLC <info@dlg.im>
  */
 
-import Markdown from 'markdown-it';
-import { highlight } from './highlight';
-import dialog from './plugin';
 import parse from './parser';
 import decorators from './decorators';
-
-const md = new Markdown('default', {
-  highlight,
-  html: false,
-  linkify: true,
-  typographer: false
-});
-
-md.use(dialog);
-
-const render = md.render.bind(md);
 
 export {
   parse,
   decorators
 };
-
-export default render;
