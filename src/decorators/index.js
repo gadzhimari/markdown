a591f32888d046501154469752fa9ad23a352423
Nikita Gusakov <gusnkt@gmail.com>
2016-11-22 18:52:39 +0300

    fix(decorators): priority and link regexp

--- src/decorators/index.js
+++ src/decorators/index.js
@@ -9,14 +9,14 @@ export const bold = createBetweenDecorator('bold', '*', true);
 export const code = createBetweenDecorator('code', '`', true);
 export const italic = createBetweenDecorator('italic', '_', true);
 
-export const link = createRegexDecorator('link', /(?:https?:\/\/){0,1}[\da-z\.-]+\.[a-z\.]{2,6}(?:\??[0-9a-z/\\#<>_-]*)*\/?/ig);
+export const link = createRegexDecorator('link', /(?:https?|ftp):\/\/[\da-z\.-]+\.[a-z\.]{2,6}(?:\??[0-9a-z/\\#<>_-]*)*\/?/ig);
 export const mention = createRegexDecorator('mention', /(?:^| )@[a-z0-9_]{5,32}/ig);
 
 const decorators = [
   code,
+  link,
   bold,
   italic,
-  link,
   mention
 ];
 
